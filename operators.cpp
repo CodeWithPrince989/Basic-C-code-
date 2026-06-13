e3eefc494a20a7860a2a968adae80bbca608dccf
Prince <princekumarsingh016592@gmail.com>
2026-06-13 19:33:02 +0530

    Initial Commit1

---
+++ operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 5;
+    cout << "+" << (a+b) << endl; //8
+    cout << "-" << (a-b) << endl;   //-2
+    cout << "*" << (a*b) << endl;   //15
+    cout << "/" << (b/a) << endl;   //1
+    cout << "%" << (a%b) << endl;   //Module (remainder) :2
+    
+    return 0;
+}
+
+
+//Symbols that tell compiler to perform some operation (a + b)
+//Type Of Operator
+// -Arithmetic => 1) Bimary(+, -, *, /, %), 2) Unary(++, --)
+// -Assignment 
+// -Relational
+// -Logical
+// -Bitwise
+
