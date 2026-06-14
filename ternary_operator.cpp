720765162e7cd6f877e22fb38fb7d4a8f64d788b
Prince <princekumarsingh016592@gmail.com>
2026-06-14 13:25:38 +0530

    switch case

---
+++ ternary_operator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isAdult;
+    int age;
+    cout<<"Enter Age: ";
+    cin>> age;
+
+    if(age>=18){
+        isAdult = true;
+    }else{
+        isAdult = false;
+    }
+
+    // Ternary Operator => variable condition? statement! : statement2; 
+    isAdult = age >= 18 ? true : false; // if age >= 18 then true else false)
+
+    //Largest of 2 numbers
+    int a = 10;
+    int b = 5;
+    int largest = a >= b ? a : b;
+    cout << "largest is "<< largest << endl;
+
+    //odd or even
+    int num = 13;
+
+    bool isEven = num % 2 == 0 ? true : false;
+    return 0;
+}
