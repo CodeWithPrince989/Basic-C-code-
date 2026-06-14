65264e400cbaa624b176d2458ecbb759f70fd9d6
Prince <princekumarsingh016592@gmail.com>
2026-06-14 12:47:44 +0530

    average in cpp

--- practice.cpp
+++ practice.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int main(){
     cout << ((bool)3 +2) << endl;
     cout << (23.5 + 2 +'A') << endl;
+    //Area of rectangle
+    int length, width;
+    cout<<("Enter Length\n")<<endl;
+    cin<<("E")
+    cout<<("Enter Breath\n")<<endl;
+    
+
+
+
     return 0;
 }
